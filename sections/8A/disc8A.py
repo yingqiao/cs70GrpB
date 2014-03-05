d42018b1176d0f6ebf6914c8d89a81dcee6f983a
Ian Ornstein <ian.ornstein@gmail.com>
2014-03-05 15:58:35 -0800

    I added the input features. Right now it simply asks for input, does not call input. Look at main method.

--- sections/8A/disc8A.py
+++ sections/8A/disc8A.py
@@ -17,6 +17,8 @@ import random
 #--------------------------
 
 
+
+
 #---------
 # HELPERS
 #---------
@@ -44,11 +46,89 @@ def pois(lmbda):
         p *= u
     return k-1
 
+# asks for type of trial. Returns "coin", "dice", or "bus"
+def trialType():
+    input = raw_input("Coin, Dice, or Bus?:  ")
+    input = input.lower()
+    if input == "coin" or input == "coins":
+        return "coin"
+    elif input == "dice" or input == "die":
+        return "dice"
+    elif input == "bus" or input == "busses":
+        return "bus"
+    else:
+        print "Please choose either coin, dice, or bus."
+        return trialType()
+
+    
+def getParametersForCoin():
+    input = raw_input("How many flips?  ")
+    try:
+        n = int(input)
+    except ValueError:
+        print "Please enter a positive integer."
+        return getParametersForCoin()
+    if n <= 0:
+        print "Please enter a positive integer."
+        return getParametersForCoin()
+    return [n, 0.5]
+
+def getParametersForDice():
+    input = raw_input("How many sides does the dice have?  ")
+    try:
+        sides = int(input)
+    except ValueError:
+        print "Please enter a positive integer."
+        return getParametersForDice()
+    if sides <= 0:
+        print "Please enter a positive integer."
+        return getParametersForDice()
+    properInput = False
+    while not properInput:
+        input = raw_input("How many rolls of the dice?  ")
+        try:
+            n = int(input)
+        except ValueError:
+            print "Please enter a positive integer."
+            continue
+        if n <= 0:
+            print "Please enter a positive integer."
+            continue
+        properInput = True
+        p = 1.0 / sides
+    return [n, p]
+
+    
+def getParametersForBus():
+    input = raw_input("How many busses pass by in 20 minutes?  ")
+    try:
+        lmbda = int(input)
+    except ValueError:
+        print "Please enter a positive integer."
+        return getParametersForBus()
+    if lmbda <= 0:
+        print "Please enter a positive integer."
+        return getParametersForBus()
+    return [lmbda]
+
+
+# gets parameters for the trial
+def getParameters(type):
+    if type == "coin":
+        return getParametersForCoin()
+    if type == "dice":
+        return getParametersForDice()
+    if type == "bus":
+        return getParametersForBus()
+
 
 #------
 # MAIN
 #------
 
+#unflag below
+#param = getParameters(trialType())
+        
 
 L = []
 for i in range(1000):
