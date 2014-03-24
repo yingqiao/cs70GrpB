e251c0261f54590bf946db12887dbcb16cce3c54
sara.sheehan <sara.sheehan@gmail.com>
2014-03-23 18:28:30 -0700

    adding more hw9 solutions

--- hws/9/hw9.py
+++ hws/9/hw9.py
@@ -12,6 +12,15 @@ import sys
 
 
 #---------
+# GLOBALS
+#---------
+
+
+FONTSIZE = 24
+COLORS = ['b','k','g','r']
+
+
+#---------
 # HELPERS
 #---------
 
@@ -48,60 +57,55 @@ def KL(a,p):
 
 
 # Q1 part (a)
-def partA(p=0.7, k=1000, m=1000):
-    print('Question 1 part (a):')
-    print('Probability of head: %f'%p)
-    print('Number of tosses: %i'%k)
-    print('Number of trials: %i'%m)
+def partA(p=0.7, krange=[10,100,1000,4000], m=10000):
 
     # plot empirical cdf
     std = math.sqrt(p*(1-p))
-    results = {}
-    results[k] = [(Sk-k*p)/(math.sqrt(k)*std) for Sk in runManyTrials(p, k, m)]
-    results[k].sort()
-    plt.plot(results[k],[float(y)/m for y in range(1,m+1)],label='k=' + str(k))
+    for k in krange:
+        results = {}
+        results[k] = [(Sk-k*p)/(math.sqrt(k)*std) for Sk in runManyTrials(p, k, m)]
+        results[k].sort()
+        plt.plot(results[k],[float(y)/m for y in range(1,m+1)],label='k=' + str(k))
         
     # overlay normal cdf
     num_points = 1000
     min_max = 3
     x_values = [float(x)/num_points for x in range(-min_max*num_points,min_max*num_points)]
-    plt.plot(x_values,[0.5 + 0.5*math.erf(x/math.sqrt(2)) for x in x_values],label='normal CDF')
+    plt.plot(x_values,[0.5 + 0.5*math.erf(x/math.sqrt(2)) for x in x_values],lw=3,label='normal CDF')
 
     # set up plot
     plt.legend(loc=2)
-    plt.xlabel('Normalized and centered fraction of heads',fontsize='large')
-    plt.ylabel('Frequency',fontsize='large')
+    plt.xlabel('Normalized and centered fraction of heads',fontsize=FONTSIZE)
+    plt.ylabel('Frequency',fontsize=FONTSIZE)
     plt.xlim(-min_max,min_max)
-    plt.title('k = %s, p = %.1f'%(k,p))
+    plt.title('p=' + str(p) + ', k = ' + str(krange) + ', m=' + str(m),fontsize=FONTSIZE)
     plt.show()
 
 
 # Q1 part (c)
-def partC(p=0.7, k=5000, m=1000):
-    print('Question 1 part (c):')
-    print('Probability of head: %f'%p)
-    print('Number of tosses: %i'%k)
-    print('Number of trials: %i'%m)
+def partC(p=0.7, krange=[10,100,1000,4000], m=10000):
+    bin_sizes = [10,10,20,24]
 
-    # plot histogram
+    # plot histograms
     std = math.sqrt(p*(1-p))
-    results = [(Sk-k*p)/(math.sqrt(k)*std) for Sk in runManyTrials(p, k, m)]
-    plt.hist(results, bins=24, align='mid', normed=True)
+    for k in krange:
+        results = [(Sk-k*p)/(math.sqrt(k)*std) for Sk in runManyTrials(p, k, m)]
+        plt.hist(results, bins=bin_sizes[krange.index(k)], align='mid', normed=True, label='k=' + str(k))
 
     # overlay normal distribution
     mean = 0
     variance = 1
     sigma = np.sqrt(variance)
     x = np.linspace(-3,3,100)
-    plt.plot(x,mlab.normpdf(x,mean,sigma),label='normal PDF')
+    plt.plot(x,mlab.normpdf(x,mean,sigma),lw=3,label='normal PDF')
 
     # set up plot
     plt.legend()
-    plt.xlabel('Number of heads',fontsize='large')
-    plt.ylabel('Frequency',fontsize='large')
+    plt.xlabel('Number of heads',fontsize=FONTSIZE)
+    plt.ylabel('Frequency',fontsize=FONTSIZE)
     min_max = 3
     plt.xlim(-min_max,min_max)
-    plt.title('k = %i, p = %.1f'%(k,p))
+    plt.title('p=' + str(p) + ', k = ' + str(krange) + ', m=' + str(m),fontsize=FONTSIZE)
     plt.show()
 
 
@@ -109,39 +113,38 @@ def partC(p=0.7, k=5000, m=1000):
 def partD(prange=[0.3,0.7], krange=range(10,201), m=10000):
     log_thresh = 1e-3
     erange=[0.05,0.1]
-    colors = ['b','k','g','r']
     color_idx = 0
 
     # plot fraction of heads above ak
     for p in prange:
-        allTrials = []
+        all_trials = []
         for k in krange:
             trials_k = runManyTrials(p, k, m)
-            allTrials.append(trials_k)
+            all_trials.append(trials_k)
         arange = [p+e for e in erange]
         for a in arange:
-            fracLst = []
+            frac_lst = []
             for i in range(len(krange)):
                 k = krange[i]
-                fracAbove = [x > a*k for x in allTrials[i]].count(True)/float(m)
-                fracLst.append(max(fracAbove,log_thresh))
+                frac_above = [Sk > a*k for Sk in all_trials[i]].count(True)/float(m)
+                frac_lst.append(max(frac_above,log_thresh))
             lbl = 'p=' + str(p) + ', a=' + str(a)
-            plt.plot(krange,fracLst,colors[color_idx] + '.-',label=lbl)
+            plt.plot(krange,frac_lst,COLORS[color_idx] + '.-',label=lbl)
 
             # fit a line to each set of points
-            ls_line = np.polyfit(krange,[math.log(f) for f in fracLst],1)
-            lbl = 'fitted line, slope=' + str("%.5f" % ls_line[0])
-            plt.plot(krange,[math.exp(ls_line[0]*k + ls_line[1]) for k in krange],colors[color_idx],label=lbl)
+            ls_line = np.polyfit(krange,[math.log(f) for f in frac_lst],1)
+            lbl = 'fitted line'
+            plt.plot(krange,[math.exp(ls_line[0]*k + ls_line[1]) for k in krange],COLORS[color_idx],label=lbl)
 
             # overlay KL divergence
-            lbl = 'KL divergence: ' + str("%.5f" % -KL(a,p))
-            plt.plot(krange,[math.exp(-KL(a,p)*k) for k in krange],colors[color_idx] + '--',label=lbl)
+            lbl = 'exp(-KL divergence)'
+            plt.plot(krange,[math.exp(-KL(a,p)*k) for k in krange],COLORS[color_idx] + '--',label=lbl)
             color_idx += 1
 
     # set up plot
-    plt.xlabel('Number of coin flips, k',fontsize='large')
-    plt.ylabel('Fraction of trials with more than a*k heads',fontsize='large')
-    plt.title('p = ' + str(prange) + ', m=' + str(m))
+    plt.xlabel('Number of coin flips, k',fontsize=FONTSIZE)
+    plt.ylabel('Fraction of trials with more than a*k heads',fontsize=FONTSIZE)
+    plt.title('p = ' + str(prange) + ', m=' + str(m),fontsize=FONTSIZE)
     plt.yscale('symlog', linthreshy=log_thresh*math.sqrt(1e-1))
     plt.axis([krange[0]-5,krange[-1]+5,log_thresh*math.sqrt(1e-1),1])
     plt.legend(loc=3)
@@ -149,16 +152,43 @@ def partD(prange=[0.3,0.7], krange=range(10,201), m=10000):
 
 
 # Q1 part (e)
-def partE(p=0.7, k=5000, m=1000):
-    print('todo')
+def partE(p=0.5, krange=range(10,201), erange=[0.1,0.2,0.3], m=10000):
+    colors = ['b','k','g']
+    color_idx = 0
+
+    all_trials = []
+    for k in krange:
+        trials_k = runManyTrials(p, k, m)
+        all_trials.append(trials_k)
+  
+    for e in erange:
+
+        # plot the fraction of trials where |S_k - kp| >= epsilon*k
+        frac_lst = []
+        for k in krange:
+            frac_close = [abs(Sk - k*p) >= e*k for Sk in trials_k].count(True)/float(m)
+            frac_lst.append(frac_close)
+        plt.plot(krange,frac_lst,COLORS[color_idx],label='epsilon=' + str(e) + ', frac of trials')
+
+        # overlay Chebyshev bound
+        plt.plot(krange,[p*(1-p)/(k*e*e) for k in krange],colors[color_idx] + '--',label='Chebyshev bound')
+        color_idx += 1
+
+    # set up plot
+    plt.legend(loc=9)
+    plt.axis([krange[0]-5,krange[-1]+5,0,0.5])
+    plt.xlabel('Number of coin flips, k',fontsize=FONTSIZE)
+    plt.ylabel("Chebyshev's inequality",fontsize=FONTSIZE)
+    plt.title('p=' + str(p) + ', m=' + str(m),fontsize=FONTSIZE)
+    plt.show()
 
 
 # Q1 part (f)
 def partF(n=50):
     plt.plot(range(0,n+1),[choose(n,k) for k in range(0,n+1)])
-    plt.xlabel('k',fontsize='large')
-    plt.ylabel('n choose k',fontsize='large')
-    plt.title('n = %i'%n)
+    plt.xlabel('k',fontsize=FONTSIZE)
+    plt.ylabel('n choose k',fontsize=FONTSIZE)
+    plt.title('n = %i'%n,fontsize=FONTSIZE)
     plt.show()
 
 
